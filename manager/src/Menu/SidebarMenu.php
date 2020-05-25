1889e275738556890dfb4f8d5d660f114e760e2a
Codenix <codenix.sv@gmail.com>
2020-05-25 23:00:26 +0300

    Added KNP Menu

---
+++ manager/src/Menu/SidebarMenu.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class SidebarMenu
+{
+    private FactoryInterface $factory;
+    private AuthorizationCheckerInterface $auth;
+
+    public function __construct(FactoryInterface $factory, AuthorizationCheckerInterface $auth)
+    {
+        $this->factory = $factory;
+        $this->auth = $auth;
+    }
+
+    public function build(): ItemInterface
+    {
+        $menu = $this->factory->createItem('root')
+            ->setChildrenAttributes(['class' => 'c-sidebar-nav']);
+
+        $menu->addChild('Dashboard', ['route' => 'home'])
+            ->setExtra('icon', 'c-sidebar-nav-icon fas fa-tachometer-alt')
+            ->setAttribute('class', 'c-sidebar-nav-item')
+            ->setLinkAttribute('class', 'c-sidebar-nav-link');
+
+        $menu->addChild('Users', ['route' => 'users'])
+            ->setExtra('icon', 'c-sidebar-nav-icon fas fa-user-cog')
+            ->setExtra('routes', [
+                ['route' => 'users'],
+                ['pattern' => '/^users\..+/']
+            ])
+            ->setAttribute('class', 'c-sidebar-nav-item')
+            ->setLinkAttribute('class', 'c-sidebar-nav-link');
+
+        $menu->addChild('Profile', ['route' => 'profile'])
+            ->setExtra('icon', 'c-sidebar-nav-icon fas fa-user-circle')
+            ->setExtra('routes', [
+                ['route' => 'profile'],
+                ['pattern' => '/^profile\..+/']
+            ])
+            ->setAttribute('class', 'c-sidebar-nav-item')
+            ->setLinkAttribute('class', 'c-sidebar-nav-link');
+
+        return $menu;
+    }
+}
