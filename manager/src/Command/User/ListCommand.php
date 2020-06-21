4b1b42c5e34c2d7751fae81100380b61d7155c93
codenix-sv <codenix.sv@gmail.com>
2020-06-21 18:56:41 +0300

    Removed unused clases

--- manager/src/Command/User/ListCommand.php
+++ manager/src/Command/User/ListCommand.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Command\User;
 
-use App\Model\User\Entity\User\User;
 use App\ReadModel\User\UserFetcher;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
